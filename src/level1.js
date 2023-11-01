cf38ae15f6d624d958b806d979ccc65714461c32
Adriaan63 <Adcast07@ucm.es>
2023-11-01 11:23:15 +0100

    empezando con el menú guarruzo

---
+++ src/level1.js
@@ -0,0 +1,14 @@
+export default class Level1 extends Phaser.Scene {
+    constructor() {
+      super({ key: 'level1' });
+      //para poder hacer el evento
+    //   this.emitter = EventDispatcher.getInstance();
+    //   this.previousLetterTime = 0;
+    //   this.introFinished = false;
+    //   this.maxVol = 0.7;
+    //   this.muteVol = 0;
+    //   this.isMute = false;
+  
+    }
+    
+}
