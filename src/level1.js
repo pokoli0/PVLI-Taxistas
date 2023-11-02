f8eec41dc90992c73b621f1c85a5cb9b24fa6f9f
Adriaan63 <Adcast07@ucm.es>
2023-11-02 22:28:26 +0100

    intentado que se ponga el mapa

--- src/level1.js
+++ src/level1.js
@@ -10,5 +10,22 @@ export default class Level1 extends Phaser.Scene {
     //   this.isMute = false;
   
     }
+    preload(){
+      this.load.tilemapTiledJSON('tilemap', 'assets/Mapas/map.json');
+      this.load.image('patronesTilemap', 'assets/CP_V1.1.0_nyknck/tileset/CP_V1.0.4.png');
+    }
+    create(){
+      this.map = this.make.tilemap({
+        key: 'tilemap',
+        tileWidth: 64, 
+        tileHeight: 64 
+      });
+      const tileset1 = this.map.addTilesetImage('assets/Mapas/map.json', 'assets/CP_V1.1.0_nyknck/tileset/CP_V1.0.4.png')
+      this.hierba = this.map.createLayer('hierba', tileset1);
+      this.casas = this.map.createLayer('casas', tileset1);
+      this.cesped = this.map.createLayer('cesped', tileset1);
+      this.asfalto = this.map.createLayer('asfalto', tileset1);
+      this.carretera = this.map.createLayer('carretera', tileset1);
+    }
     
 }
