4da34a9c6114ce1134d482517d2a79a580ccc4f9
nico-best1 <nicobest@ucm.es>
2023-11-03 01:44:02 +0100

    la camara sigue al coche

--- src/level1.js
+++ src/level1.js
@@ -29,13 +29,11 @@ export default class Level1 extends Phaser.Scene {
       this.casas = this.map.createLayer('casas', tileset1);
       this.hierba = this.map.createLayer('hierba', tileset1);
       // Establece los límites de la cámara para que se ajusten al tamaño del mapa
-      this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
-
-      // Ajusta la cámara para que muestre todo el mapa
-      this.cameras.main.scrollX = 0;  // Desplazamiento horizontal
-      this.cameras.main.scrollY = 0;  // Desplazamiento vertical    
+      this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);   
 
       this.car = new Car(this, 100, 100, 'Coche');
+
+      this.cameras.main.startFollow(this.car, true, 0.1, 0.1);
 
     }
 
