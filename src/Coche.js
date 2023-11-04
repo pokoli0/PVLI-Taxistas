d2e7e8401b62f3117971962370bfab31b88f4778
Adriaan63 <Adcast07@ucm.es>
2023-11-04 16:22:23 +0100

    colliders pochos

--- src/Coche.js
+++ src/Coche.js
@@ -1,6 +1,6 @@
 export default class Car extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, textureUp, textureDown, textureRight, textureLeft) {
-      super(scene, x, y, textureDown);
+      super(scene, x, y, textureRight);
       scene.add.existing(this);
       scene.physics.world.enable(this);
       this.keys = scene.input.keyboard.addKeys({
@@ -14,34 +14,64 @@ export default class Car extends Phaser.GameObjects.Sprite {
       this.textureDown = textureDown;
       this.textureRight = textureRight;
       this.textureLeft = textureLeft;
+      
+      // Ancho y alto del collider general, ajusta estos valores según tu vehículo
+      const colliderWidth = 50;
+      const colliderHeight = 50;
+
+      // Crea el collider general como un rectángulo alrededor del vehículo
+      this.collider = scene.physics.add.sprite(x, y).setSize(colliderWidth, colliderHeight);
+      this.collider.visible = false; // Haz que el collider no sea visible en el juego
     }  
-    update() {    
+    update() {
+      this.collider.setPosition(this.x, this.y);
       var accel;
       if (this.keys.shift.isDown) {
-        accel =2;
-      }
-      else{
+        accel = 2;
+      } else {
         accel = 1;
       }
+    
+      let textureSet = false; // Variable para controlar si se ha configurado una textura
+    
       if (this.keys.up.isDown) {
-        this.setTexture(this.textureUp); 
-        this.body.setVelocityY(-100*accel); 
+        if (this.keys.left.isDown) {
+          this.setTexture(this.textureUp);
+        } else if (this.keys.right.isDown) {
+          this.setTexture(this.textureUp);
+        } else {
+          this.setTexture(this.textureUp);
+        }
+        this.body.setVelocityY(-100 * accel);
+        textureSet = true;
       } else if (this.keys.down.isDown) {
-        this.setTexture(this.textureDown); 
-        this.body.setVelocityY(100*accel); 
+        if (this.keys.left.isDown) {
+          this.setTexture(this.textureDown);
+        } else if (this.keys.right.isDown) {
+          this.setTexture(this.textureDown);
+        } else {
+          this.setTexture(this.textureDown);
+        }
+        this.body.setVelocityY(100 * accel);
+        textureSet = true;
       } else {
-        this.body.setVelocityY(0); 
+        this.body.setVelocityY(0);
       }
-  
-      if (this.keys.left.isDown) {                    
-        this.setTexture(this.textureLeft); 
-        this.body.setVelocityX(-100*accel); 
-      } else if (this.keys.right.isDown) {   
-        this.setTexture(this.textureRight); 
-        this.body.setVelocityX(100*accel);
+    
+      if (this.keys.left.isDown) {
+        if (!textureSet) {
+          this.setTexture(this.textureLeft);
+        }
+        this.body.setVelocityX(-100 * accel);
+      } else if (this.keys.right.isDown) {
+        if (!textureSet) {
+          this.setTexture(this.textureRight);
+        }
+        this.body.setVelocityX(100 * accel);
       } else {
-        this.body.setVelocityX(0); 
+        this.body.setVelocityX(0);
       }
     }
+
   }
   
--- src/level1.js
+++ src/level1.js
@@ -9,8 +9,12 @@ export default class Level1 extends Phaser.Scene {
     //   this.maxVol = 0.7;
     //   this.muteVol = 0;
     //   this.isMute = false;
+    
   
     }
+    // handleCollision(car, colisiones){
+    //   car.setVelocity(0,0);
+    // }
     preload(){
       this.load.tilemapTiledJSON('tilemap', 'assets/Mapas/mapa1.json');
       this.load.image('patronesTilemap', 'assets/CP_V1.1.0_nyknck/tileset/CP_V1.0.4.png');
@@ -20,29 +24,13 @@ export default class Level1 extends Phaser.Scene {
       this.load.image('TaxiRight', 'assets/sprites/taxi3.png');
     }
     create(){
-      this.map = this.make.tilemap({
-        key: 'tilemap',
-        tileWidth: 64, 
-        tileHeight: 64 
-      });
-      const tileset1 = this.map.addTilesetImage('level1', 'patronesTilemap');
-      this.carretera = this.map.createLayer('carretera', tileset1);
-      this.asfalto = this.map.createLayer('asfalto', tileset1);
-      this.cesped = this.map.createLayer('cesped', tileset1);
-      this.casas = this.map.createLayer('casas', tileset1);
-      this.hierba = this.map.createLayer('hierba', tileset1);
-      this.colisiones = this.map.createLayer('colisiones', tileset1);
+      this.createTileMap();
       // Establece los límites de la cámara para que se ajusten al tamaño del mapa
-    
-      this.colisiones.setCollisionByProperty({ colisiona: true });
-      
-
       this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
       this.physics.world.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
       this.physics.world.gravity.y = 0; // Esto desactiva la gravedad en el eje Y, puedes ajustarlo según tus necesidades
 
       this.car = new Car(this, 450, 120, 'TaxiUp', 'TaxiDown', 'TaxiRight','TaxiLeft');
-      this.physics.world.enable(this.car);
       
       this.physics.add.collider(this.car, this.colisiones);
       this.colisiones.setCollisionBetween(131,133);
@@ -53,6 +41,25 @@ export default class Level1 extends Phaser.Scene {
 
     update(){
       this.car.update();
+
+    }
+    
+
+    createTileMap(){
+      this.map = this.make.tilemap({
+        key: 'tilemap',
+        tileWidth: 64, 
+        tileHeight: 64 
+      });
+      const tileset1 = this.map.addTilesetImage('level1', 'patronesTilemap');
+      this.carretera = this.map.createLayer('carretera', tileset1);
+      this.asfalto = this.map.createLayer('asfalto', tileset1);
+      this.cesped = this.map.createLayer('cesped', tileset1);
+      this.casas = this.map.createLayer('casas', tileset1);
+      this.casas.setDepth(2);
+      this.hierba = this.map.createLayer('hierba', tileset1);
+      this.hierba.setDepth(3);
+      this.colisiones = this.map.createLayer('colisiones', tileset1);
     }
     
 }
