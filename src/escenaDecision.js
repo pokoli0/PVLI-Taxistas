5f76ba5093b68e1a21ee4eb8052e9161dd433271
Adriaan63 <Adcast07@ucm.es>
2023-12-19 23:07:56 +0100

    otra vez los mensajes

--- src/escenaDecision.js
+++ src/escenaDecision.js
@@ -34,12 +34,26 @@ export default class escenaDecision extends Phaser.Scene{
             if (!this.asesino) {
                 this.puntos += 25; // Incrementa los puntos en 50
                 this.actualizarPuntos(); // Actualiza el objeto de texto
+                this.add.text(230, 170, //Posicion de las preguntas (CAMBIAR ESTO)
+                "Has enviado a un ángel al cielo, felicidades.", 
+                { 
+                    fill: '#ffffff',  //Color del texto de las preguntas
+                    fontFamily: "VT323",
+                    fontSize: '30px'
+                });
             }else{
                 this.puntos -= 15; // Incrementa los puntos en 50
                 this.actualizarPuntos(); // Actualiza el objeto de texto
+                this.add.text(200, 133, //Posicion de las preguntas (CAMBIAR ESTO)
+                "No ha sido tu mejor decision.", 
+                { 
+                    fill: '#ffffff',  //Color del texto de las preguntas
+                    fontFamily: "VT323",
+                    fontSize: '30px'
+                });
             }
             botonCielo.disableInteractive();
-            this.time.delayedCall(1000, () => {
+            this.time.delayedCall(3000, () => {
             const controlLevelsScene = this.scene.get('controlLevels', {puntos: this.puntos});
             // Llamar al método avanzarAlSiguienteNivel() de la escena de controlLevels
             controlLevelsScene.avanzarAlSiguienteNivel(this.puntos);
@@ -59,12 +73,26 @@ export default class escenaDecision extends Phaser.Scene{
             if (this.asesino) {
                 this.puntos += 25; // Incrementa los puntos en 50
                 this.actualizarPuntos(); // Actualiza el objeto de texto
+                this.add.text(200, 133, //Posicion de las preguntas (CAMBIAR ESTO)
+                "¡Felicidades! Ha ganado un boleto de ida al infierno.\nQue no olvide llevar protector solar.", 
+                { 
+                    fill: '#ffffff',  //Color del texto de las preguntas
+                    fontFamily: "VT323",
+                    fontSize: '30px'
+                });
             }else{
                 this.puntos -= 15; // Incrementa los puntos en 50
                 this.actualizarPuntos(); // Actualiza el objeto de texto
+                this.add.text(200, 133, //Posicion de las preguntas (CAMBIAR ESTO)
+                "Fallaste, ten cuidado esto podria tener consecuencias.",
+                {
+                    fill: '#ffffff',  //Color del texto de las preguntas
+                    fontFamily: "VT323",
+                    fontSize: '30px'
+                });
             }
             botonInf.disableInteractive();
-            this.time.delayedCall(1000, () => {
+            this.time.delayedCall(3000, () => {
             const controlLevelsScene = this.scene.get('controlLevels');
             // Llamar al método avanzarAlSiguienteNivel() de la escena de controlLevels
             controlLevelsScene.avanzarAlSiguienteNivel(this.puntos);
@@ -78,6 +106,23 @@ export default class escenaDecision extends Phaser.Scene{
     botonVivir(){
         const dejarVivo= this.add.sprite(500, 400, 'dejarVivo').setInteractive();
         dejarVivo.setScale(0.3);
-        
+        dejarVivo.on('pointerdown', () => {
+            this.add.text(200, 133, //Posicion de las preguntas (CAMBIAR ESTO)
+                "Su destino pende de un hilo.\nLa vida le ha dado una segunda oportunidad.",
+                {
+                    fill: '#ffffff',  //Color del texto de las preguntas
+                    fontFamily: "VT323",
+                    fontSize: '30px'
+                });
+            dejarVivo.disableInteractive();
+            this.time.delayedCall(3000, () => {
+            const controlLevelsScene = this.scene.get('controlLevels');
+            // Llamar al método avanzarAlSiguienteNivel() de la escena de controlLevels
+            controlLevelsScene.avanzarAlSiguienteNivel(this.puntos);
+            // Detener la escena actual
+            this.scene.stop('escenaDecision');
+            // Pasar los puntos a la escena de controlLevels
+            });
+        });
     }
 }
