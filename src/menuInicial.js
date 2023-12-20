e33f0db2af6840f499cce6982816edce050f7374
Adriaan63 <Adcast07@ucm.es>
2023-12-20 12:21:59 +0100

    titulo

--- src/menuInicial.js
+++ src/menuInicial.js
@@ -11,6 +11,7 @@ export default class Menu extends Phaser.Scene {
         //Ponemos imagenes:
         const background = this.add.video(500, 250, 'FondoMenu');
         background.play(true); // Reproduce el video en bucle
+        background.setAlpha(0.7);
         const button = this.add.image(this.scale.width / 2, this.scale.height / 1.6, 'Play').setInteractive().setScale(0.4); 
         const controlesButton = this.add.image(this.scale.width / 2, this.scale.height / 1.25, 'ControlesButton').setInteractive().setScale(0.35); //Boton de CONTROLES
         const ButtonClicked = this.sound.add('Button');
@@ -21,7 +22,7 @@ export default class Menu extends Phaser.Scene {
         musica.play({
             loop: true //para que este en bucle
         })*/
-
+        this.ponerTitulo();
         //Conexion con escena level (al pulsar boton)
         button.on("pointerdown", () => {
             ButtonClicked.play();
@@ -52,5 +53,17 @@ export default class Menu extends Phaser.Scene {
     controlesButton.on('pointerout', () => {
         controlesButton.setScale(0.35);
     });
+    }
+
+    ponerTitulo(){
+        console.log("Titulo")
+        this.add.text(370, 20, //Posicion de las preguntas (CAMBIAR ESTO)
+                    'DEATH\nCAB', 
+                    { 
+                        fill: '#ffe00f',  //Color del texto de las preguntas
+                        fontFamily: "VT323",
+                        fontSize: '130px',
+                        align: 'center'
+                    });
     }
   }
