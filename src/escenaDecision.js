8623265c8cb41f4de5dfd615096a060638ab9b81
Nicolás Besteiro <nicobest@ucm.es>
2023-12-20 13:16:04 +0100

    Texto pregunta de donde mandarlo

--- src/escenaDecision.js
+++ src/escenaDecision.js
@@ -23,8 +23,16 @@ export default class escenaDecision extends Phaser.Scene{
         const moneda = this.add.sprite(40, 40, 'moneda');
         moneda.setScale(0.15);
         this.textoPuntos = this.add.text(moneda.x,
-            moneda.y, this.puntos, { fontSize: '48px', fill: '#000' , align: 'center',});
+            moneda.y, this.puntos, { fontSize: '60px', fill: '#fff' ,  fontFamily: "VT323", align: 'center',});
         this.textoPuntos.setOrigin(0.5);
+        this.textoPregunta = this.add.text(90, 100, '¿Qué quieres hacer con el cliente?', {
+                fontSize: '80px', 
+                fontFamily: "VT323",
+                fill: '#fff' , 
+                align: 'center',
+            }
+        );
+        this.textoPregunta.setScale(0.8);
         this.botonCielo();
         this.botonInfierno();
         this.botonVivir();
@@ -36,6 +44,7 @@ export default class escenaDecision extends Phaser.Scene{
         const botonCielo= this.add.sprite(250, 400, 'botonCielo').setInteractive();
         botonCielo.setScale(0.3);
         botonCielo.on('pointerdown', () => {
+            this.textoPregunta.setVisible(false);
             this.ButtonClicked.play();
             if (!this.asesino) {
                 this.puntos += 25; // Incrementa los puntos en 50
@@ -84,6 +93,7 @@ export default class escenaDecision extends Phaser.Scene{
         const botonInf= this.add.sprite(750, 400, 'botonInf').setInteractive();
         botonInf.setScale(0.3);
         botonInf.on('pointerdown', () => {
+            this.textoPregunta.setVisible(false);
             this.ButtonClicked.play();
             if (this.asesino) {
                 this.puntos += 25; // Incrementa los puntos en 50
@@ -129,6 +139,7 @@ export default class escenaDecision extends Phaser.Scene{
         const dejarVivo= this.add.sprite(500, 400, 'dejarVivo').setInteractive();
         dejarVivo.setScale(0.3);
         dejarVivo.on('pointerdown', () => {
+            this.textoPregunta.setVisible(false);
             this.ButtonClicked.play();
             this.add.text(200, 133, //Posicion de las preguntas (CAMBIAR ESTO)
                 "Su destino pende de un hilo.\nLa vida le ha dado una segunda oportunidad.",
